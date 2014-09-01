90cc2cd5a35bb967ef891c3b3f2067e2bc547b9e
KevinMarete <kevomarete@gmail.com>
2014-09-01 19:09:10 +0300

    Assets folder missing 1.Updated changelog file. 2.Updated readme file.

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,4 +1,2 @@
-<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14092128771cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
-<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1409212876ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
-
-
+<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14095495401cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
+<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1409549540ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
