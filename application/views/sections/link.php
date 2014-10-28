ebb7909bed8a2eb0a6cb45287ad3728e747de656
unknown <Kevin.Marete@VDI-HP_LAB-5.strathmore.local>
2014-10-28 10:43:19 +0300

    Merge With Abdala added duplicate variable in order

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,2 +1,2 @@
-<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14127607341cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
-<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1413884968ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
+<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14141362971cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
+<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1414481200ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
