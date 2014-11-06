e68b9859dd46559ac345d71ddb06af3e51d0dea2
unknown <Kevin.Marete@VDI-HP-LAB-5.strathmore.local>
2014-11-06 08:38:39 +0000

    merged with abdala

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,2 +1,2 @@
-<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14141362971cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
-<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1414481200ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
+<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14151749471cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
+<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1415174947ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
