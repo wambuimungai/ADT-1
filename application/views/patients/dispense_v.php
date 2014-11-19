eb85ad193ca27c028e5d1300660afd2ded54a4a1
Joy Mungai <ijoy.mungai@gmail.com>
2014-11-19 11:45:06 +0300

    remove brand name column and confirm reset in dispensing and inventory

--- application/views/patients/dispense_v.php
+++ application/views/patients/dispense_v.php
@@ -248,7 +248,7 @@
                         <th>Duration</th>
                         <th style="width:5%">Qty. disp</th>
                         <th style="width:8%">Stock on Hand</th>
-                        <th>Brand Name</th>
+<!--                        <th>Brand Name</th>-->
                         <th>Indication</th>
                         <th>Comment</th>
                         <th>Missed Pills</th>
@@ -285,7 +285,7 @@
                             <input type="text" name="soh[]" class="soh input-small" readonly="readonly"/>
                         </td>
                         </td>
-                        <td><select name="brand[]" class="brand input-small"></select></td>
+<!--                        <td><select name="brand[]" class="brand input-small"></select></td>-->
 
                         <td>
                             <select name="indication[]" class="indication input-small " style="">
@@ -1186,10 +1186,14 @@
         }
     });
     $("#reset").click(function (e){
-        e.preventDefault();
+    e.preventDefault();
+    bootbox.confirm("<h4>Reset?</h4>\n\<hr/><center>Are you sure?</center>", function(res){
+        if(res)
         reinitialize();
         clearForm("#dispense_form");
         resetRoutineDrugs();
+    });
+       
     });
 	
 	//-------------------------------- ADD, REMOVE, RESET END ----------------------------
--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,3 +1,2 @@
 <link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14150076831cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
-<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1415007676ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
-
+<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1416249009ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
