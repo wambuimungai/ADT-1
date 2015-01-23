d57bb9d99b5e96e93432d72e6098e30382023186
unknown <Rufus.Mbugua@VDI-HP-LAB-4.strathmore.local>
2015-01-23 14:03:29 +0000

    few edits

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,3 +1,2 @@
-<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14108764721cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
-<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1417598114ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
-
+<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14212206781cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
+<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1421220678ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
